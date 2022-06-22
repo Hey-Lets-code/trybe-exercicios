9b03e5c15744954142cff95a3a9f75e5317f9727
leticia <le.augusta.souza@gmail.com>
2022-06-22 11:49:45 -0300

    Exercício de switch case

---
+++ switch-case-exercise.js
@@ -0,0 +1,19 @@
+let trafficLight = "amarelo";
+
+switch (trafficLight) {
+    
+case "vermelho":
+    console.log("pare")
+    break
+
+    case "amarelo":
+    console.log("atenção")
+    break
+
+    case "verde":
+    console.log ("siga")
+    break
+
+    default:
+        console.log ('Valor não identificado')
+}
