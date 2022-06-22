9a021bdbbc9824cb400df06c728028cf0df70900
leticia <le.augusta.souza@gmail.com>
2022-06-21 22:38:49 -0300

    Exercícios das aulas

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const myName = ("Letícia");
+const birthCity = ("Jaboatão");
+var birthYear = 1999;
+birthYear = 2030;
+birthCity = ("Recife")
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
