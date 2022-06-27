2321ea83210190b6df120258184d0b99a93d900b
leticia <le.augusta.souza@gmail.com>
2022-06-27 10:15:27 -0300

    Exercicio: 4 - dia: 1 - bloco: 4

---
+++ exercicio-4.js
@@ -0,0 +1,9 @@
+const value = 50;
+
+if (value > 0) {
+    console.log('Positivo! O valor é:', value)
+}else if (value === 0) {
+    console.log('Nulo! O valor é:', value);
+} else {
+    console.log('Negativo! O valor é:', value);
+}
