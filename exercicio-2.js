2f43925cc0fcf1e8ec13c293a87f97b271de52bf
leticia <le.augusta.souza@gmail.com>
2022-06-27 10:02:26 -0300

    Exercicio:2 - dia: 1 - Bloco: 4

---
+++ exercicio-2.js
@@ -0,0 +1,8 @@
+let numberOne = 42;
+let numberTwo = 54;
+
+if (numberOne > numberTwo) {
+    console.log("O número um é maior que o número dois.")
+}else{
+    console.log("O número dois é maior que o número um.")
+}
