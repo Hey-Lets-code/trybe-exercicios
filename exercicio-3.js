0316f001f7425f13cc57e41d1caf73f9de4ff166
leticia <le.augusta.souza@gmail.com>
2022-06-27 10:09:39 -0300

    Exercicio:3 - dia: 1 - bloco: 4

---
+++ exercicio-3.js
@@ -0,0 +1,12 @@
+const constOne = 5
+const constTwo = 6
+const constThree = 7
+
+if (constOne > constTwo && constOne > constTwo){
+    console.log('O primeiro valor é maior que o segundo e o terceiro.');
+}else if (constTwo > constOne && constTwo > constThree) {
+    console.log('O segundo número é maior que o primeiro e o terceiro.');
+}else{
+    console.log('O terceiro número é maior que o primeiro e o terceiro.');
+}
+
