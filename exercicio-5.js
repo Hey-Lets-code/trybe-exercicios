b7924ab687ce21fe2524dec2c4eda6743818893c
leticia <le.augusta.souza@gmail.com>
2022-06-27 12:03:02 -0300

    Exercicio 5 - dia 1 - bloco 4

--- exercicio-5.js
+++ exercicio-5.js
@@ -1,13 +1,35 @@
 const base = 80;
 const ladoUm = 60;
 const ladoDois = 40;
-let somaDosAngulos = base + ladoUm + ladoDois
-let t = true
-let f = false
-if (somaDosAngulos >= 180 && somaDosAngulos <= 360) {
-    console.log(t)
-} else if (somaDosAngulos < 180 && somaDosAngulos > 0){
-    console.log(f)
-}else{
-    console.log('Mensagem de erro: ângulo inválido')
+
+let somaDosAngulos = base + ladoUm + ladoDois;
+let t = true;
+let f = false;
+
+if (somaDosAngulos === 180) {
+  console.log(t);
+} else if (somaDosAngulos > 0 && somaDosAngulos < 180) {
+  console.log(f);
+} else {
+  console.log("Erro: ângulo inválido");
+}
+
+//outra solução:
+let degreeAngleA = 65;
+let degreeAngleB = 100;
+let degreeAngleC = 15;
+
+let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+let allAnglesArePositives =
+  degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+if (allAnglesArePositives) {
+  if (sumOfAngles === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+} else {
+  console.log("Erro: ângulo inválido");
 }
