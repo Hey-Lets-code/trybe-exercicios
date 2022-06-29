795b6a6f0ddcc637bead0ef8f155bfe33f1bab5c
leticia <le.augusta.souza@gmail.com>
2022-06-28 21:26:55 -0300

    Arquivos deletados, organização de pasta

---
+++ dia-1-dom-seletores-e-web-storage-a/exercicio2-aula.js
@@ -0,0 +1,4 @@
+document.getElementById("header-container").style.backgroundColor = "green"
+document.getElementsByClassName('emergency-tasks')[0].style.background = "purple"
+document.querySelector('#footer-container').style.background = "blue"
+document.querySelectorAll('.no-emergency-tasks')[0].style.background = "grey"
