360b70c244ad77394a8e44ed6d3d1f2355be314f
leticia <le.augusta.souza@gmail.com>
2022-06-28 21:30:22 -0300

    Arquivos deletados

--- dia-1-dom-seletores-e-web-storage-a/exercicio2-aula.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("header-container").style.backgroundColor = "green"
-document.getElementsByClassName('emergency-tasks')[0].style.background = "purple"
-document.querySelector('#footer-container').style.background = "blue"
-document.querySelectorAll('.no-emergency-tasks')[0].style.background = "grey"
