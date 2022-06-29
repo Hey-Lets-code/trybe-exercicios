c2a8a40cb5a9b55df8be2087e549c1cabcb07a6d
Letícia Augusta <106818612+Hey-Lets-code@users.noreply.github.com>
2022-06-28 21:16:33 -0300

    Delete scripts.js
    Deletei exercicio da branch exercicio/5.1

--- scripts.js
+++
@@ -1,9 +0,0 @@
-const myName = ("Letícia");
-const birthCity = ("Jaboatão");
-var birthYear = 1999;
-birthYear = 2030;
-birthCity = ("Recife")
-
-console.log (myName);
-console.log (birthCity);
-console.log (birthYear);
