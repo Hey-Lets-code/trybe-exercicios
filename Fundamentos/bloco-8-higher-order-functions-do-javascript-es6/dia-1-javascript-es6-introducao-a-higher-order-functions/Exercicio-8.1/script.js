fccae039ed8b0f5d1fb02a1742ffe3bc904d5476
leticia <le.augusta.souza@gmail.com>
2022-07-25 13:08:58 -0300

    Mudanças adicionadas

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/Exercicio-8.1/script.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/Exercicio-8.1/script.js
@@ -46,3 +46,125 @@ const newEmployees = (callback) => {
   }
 
   console.log(newEmployees(pessoaContratada));
+
+  // Questão 2
+
+  /* 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. 
+  Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros 
+  o número apostado e uma função que checa se o número apostado é igual ao número sorteado. 
+  O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou"). */
+  const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min
+
+  const sorteio = (callback) => {
+   return callback(1, 5);
+  }
+
+  console.log(sorteio(getRndInteger));
+
+  // Questão 3
+
+  /*  3 - Crie uma HOF que receberá três parâmetros:
+
+    O primeiro será um array de respostas corretas (Gabarito);
+    O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+    O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. 
+   
+    Para isso essa função usará os seguintes critérios:
+        Uma resposta correta adiciona 1 ponto à pontuação final;
+        A ausência de uma resposta não altera a pontuação (quando for "N.A");
+        Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+   
+        Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. 
+    Utilize os seguintes arrays:
+ */
+
+    const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+    const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+    let pontucao = 0;
+
+    const respostas = () => {
+    }
+
+    const compararArray = (callback) => {
+      return callback()
+    }
+
+    // Bônus
+
+    //PARTE 1
+
+    /* Parte I - Game Actions Simulator
+    Nestes exercícios você irá implementar HOFs que simulam um turno de batalha em um jogo. 
+    Você irá criar funções que calculam dano, atualizam status, e ao final, retornam os resultados da rodada.
+    Para os próximos exercícios copie o código abaixo. */
+    
+
+    const mage = {
+    healthPoints: 130,
+    intelligence: 45,
+    mana: 125,
+    damage: undefined,
+    };
+
+    const warrior = {
+    healthPoints: 200,
+    strength: 30,
+    weaponDmg: 2,
+    damage: undefined,
+    };
+
+    const dragon = {
+    healthPoints: 350,
+    strength: 50,
+    damage: undefined,
+    };
+
+    const battleMembers = { mage, warrior, dragon };
+
+    /*
+     1 - Crie uma função que retorna o dano do dragão.
+
+    O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+
+2 - Crie uma função que retorna o dano causado pelo warrior.
+
+    O dano será um número aleatório entre o valor do atributo strength (dano mínimo) 
+    e o valor de strength * weaponDmg (dano máximo).
+
+3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano 
+e a mana gasta pelo mago em um turno.
+
+    O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) 
+    e o valor de intelligence * 2 (dano máximo).
+    A mana consumida por turno é 15. Além disto a função deve ter uma condicional, 
+    caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem 
+    (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+
+Parte II
+Agora que você já possui a implementação das funções relativas aos três exercícios anteriores, 
+passe-as como parâmetro para outras funções que irão compor um objeto gameActions. 
+O objeto será composto por ações do jogo e cada ação é por denifição uma HOF, pois neste caso, 
+são funções que recebem como parâmetro outra função.
+Copie o código abaixo e inicie sua implementação:
+    */
+
+const gameActions = {
+  // Crie as HOFs neste objeto.
+};    
+
+/* 
+ 1 - Crie a primeira HOF que compõe o objeto gameActions. Ela será a função que simula o turno do personagem warrior. 
+ Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior 
+ e atualizará os healthPoints do monstro dragon. Além disto ela também deve atualizar o valor da chave damage do warrior.
+
+2 - Crie a segunda HOF que compõe o objeto gameActions. Ela será a função que simula o turno do personagem mage. 
+Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem mage e atualizará 
+os healthPoints do monstro dragon. Além disto ela também deve atualizar o valor das chaves damage e mana do mage.
+
+3 - Crie a terceira HOF que compõe o objeto gameActions. Ela será a função que simula o turno do monstro dragon. 
+Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo monstro dragon e atualizará os 
+healthPoints dos personagens mage e warrior. Além disto ela também deve atualizar o valor da chave damage do monstro.
+4 - Adicione ao objeto gameActions uma função que retorne o objeto battleMembers atualizado e 
+faça um console.log para visualizar o resultado final do turno.
+*/
